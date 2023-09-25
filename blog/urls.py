7eda88107aaf8b680de28d58f31eab14a54cc88e
Bowlesy666 <>
2023-09-25 23:50:38 +0000

    Add Post detail view, html, url

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     # for class based views we must use as_view() for it to be rendered
-    path('', views.PostList.as_view(), name='home')
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
