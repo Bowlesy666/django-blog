3a56da0cc7a48486b8960229310ae2a601363afa
Bowlesy666 <>
2023-09-27 00:14:28 +0000

    Commenting functionality added

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 
 class PostList(generic.ListView):
@@ -26,6 +27,44 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
-                "liked": liked
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            # set the email and name auto' from user thats logged in
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            # save comment but not commit to db yet
+            comment = comment_form.save(commit=False)
+            # first we assign a post to it
+            comment.post = post
+            # then we can save it
+            comment.save()
+        else:
+            # if form not valid we return empty comment instance
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "liked": liked,
+                "comment_form": CommentForm()
             },
         )
